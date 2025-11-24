0c966d72e3c0aea0ea74a84e3e7ea3d4f5b11485
Mac <mac@MacBook-Pro-de-Mac.local>
2025-11-23 19:52:19 -0500

    save

--- src/config/db.js
+++ src/config/db.js
@@ -4,10 +4,8 @@ import mongoose from "mongoose";
 // Función asincrona para conectar a la base de datos MongoDB
 const conectarDB = async () => {
   try {
-   await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI) 
+      console.log('Conectado a la base de datos MongoDB!!!!');
       
     } catch (error) {
       console.error("Error al conectar a la base de datos:", error);
